ae1bfe1a507dba855a49777264924aaa8d8f64f4
foryuha <foryuha2-c@my.cityu.edu.hk>
2015-05-10 13:01:30 +0800

    testing

---
+++ plot1.R
@@ -0,0 +1,6 @@
+data <- read.table("household_power_consumption.txt", header = T, sep = ";", na.strings = "?", stringsAsFactors = F)
+tmp <- as.Date(data$Date, format = "%d/%m/%Y")
+required_data = data[tmp >= as.Date("2007-02-01") & tmp <= as.Date("2007-02-02"), "Global_active_power"]
+png(filename = "plot1.png", width = 480, height = 480)
+hist(required_data, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+dev.off()
